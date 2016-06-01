b3a14e1db6bdf27254aa5245b80601bf7ceb95fc
sfsheng0322 <sfsheng0322@gmail.com>
2016-06-01 14:52:02 +0800

    init project

---
+++ MarqueeView/src/main/java/com/sunfusheng/marqueeview/MarqueeView.java
@@ -0,0 +1,135 @@
+package com.sunfusheng.marqueeview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sunfusheng on 16/5/31.
+ */
+public class MarqueeView extends ViewFlipper {
+
+    private Context mContext;
+    private List<String> notices;
+    private boolean isSetAnimDuration = false;
+
+    private int interval = 2000;
+    private int animDuration = 500;
+    private int textSize = 14;
+    private int textColor = 0xffffffff;
+
+    public MarqueeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        this.mContext = context;
+        if (notices == null) {
+            notices = new ArrayList<>();
+        }
+
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MarqueeViewStyle, defStyleAttr, 0);
+        interval = typedArray.getInteger(R.styleable.MarqueeViewStyle_mvInterval, interval);
+        isSetAnimDuration = typedArray.hasValue(R.styleable.MarqueeViewStyle_mvAnimDuration);
+        animDuration = typedArray.getInteger(R.styleable.MarqueeViewStyle_mvAnimDuration, animDuration);
+        if (typedArray.hasValue(R.styleable.MarqueeViewStyle_mvTextSize)) {
+            textSize = (int) typedArray.getDimension(R.styleable.MarqueeViewStyle_mvTextSize, textSize);
+            textSize = DisplayUtil.px2sp(mContext, textSize);
+        }
+        textColor = typedArray.getColor(R.styleable.MarqueeViewStyle_mvTextColor, textColor);
+        typedArray.recycle();
+
+        setFlipInterval(interval);
+
+        Animation animIn = AnimationUtils.loadAnimation(mContext, R.anim.anim_marquee_in);
+        if (isSetAnimDuration) animIn.setDuration(animDuration);
+        setInAnimation(animIn);
+
+        Animation animOut = AnimationUtils.loadAnimation(mContext, R.anim.anim_marquee_out);
+        if (isSetAnimDuration) animOut.setDuration(animDuration);
+        setOutAnimation(animOut);
+    }
+
+    // 根据公告字符串启动轮播
+    public void startWithText(final String notice) {
+        if (TextUtils.isEmpty(notice)) return;
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                startWithFixedWidth(notice, getWidth());
+            }
+        });
+    }
+
+    // 根据公告字符串列表启动轮播
+    public void startWithList(List<String> notices) {
+        setNotices(notices);
+        start();
+    }
+
+    // 根据宽度和公告字符串启动轮播
+    private void startWithFixedWidth(String notice, int width) {
+        int noticeLength = notice.length();
+        int dpW = DisplayUtil.px2dip(mContext, width);
+        int limit = dpW/textSize;
+        if (dpW == 0) {
+            throw new RuntimeException("Please set MarqueeView width !");
+        }
+
+        if (noticeLength <= limit) {
+            notices.add(notice);
+        } else {
+            int size = noticeLength/limit + (noticeLength%limit != 0? 1:0);
+            for (int i=0; i<size; i++) {
+                int startIndex = i*limit;
+                int endIndex = ((i+1)*limit >= noticeLength? noticeLength:(i+1)*limit);
+                notices.add(notice.substring(startIndex, endIndex));
+            }
+        }
+        start();
+    }
+
+    // 启动轮播
+    public boolean start() {
+        if (notices == null || notices.size() == 0) return false;
+        removeAllViews();
+        for (String notice:notices) {
+            addView(createTextView(notice));
+        }
+        if (notices.size() > 1) {
+            startFlipping();
+        }
+        return true;
+    }
+
+    // 创建ViewFlipper下的TextView
+    private TextView createTextView(String text) {
+        TextView tv = new TextView(mContext);
+        tv.setGravity(Gravity.LEFT|Gravity.CENTER_VERTICAL);
+        tv.setText(text);
+        tv.setTextColor(textColor);
+        tv.setTextSize(textSize);
+        return tv;
+    }
+
+    public List<String> getNotices() {
+        return notices;
+    }
+
+    public void setNotices(List<String> notices) {
+        this.notices = notices;
+    }
+
+}
---
+++ Sample/src/main/java/com/sunfusheng/sample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.sunfusheng.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.sunfusheng.marqueeview.MarqueeView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MarqueeView marqueeView1;
+    private MarqueeView marqueeView2;
+    private MarqueeView marqueeView3;
+    private MarqueeView marqueeView4;
+
+    private String KEY = "2725c35876b42c825c7d185f0052eca8d6a6cf82";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        marqueeView1 = (MarqueeView) findViewById(R.id.marqueeView1);
+        marqueeView2 = (MarqueeView) findViewById(R.id.marqueeView2);
+        marqueeView3 = (MarqueeView) findViewById(R.id.marqueeView3);
+        marqueeView4 = (MarqueeView) findViewById(R.id.marqueeView4);
+
+        marqueeView1.startWithText(getString(R.string.marquee_texts));
+        marqueeView2.startWithText(getString(R.string.marquee_texts));
+        marqueeView3.startWithText(getString(R.string.marquee_texts));
+        marqueeView4.startWithText(getString(R.string.marquee_text));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_menu_app:
+                startActivity(new Intent(this, AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
